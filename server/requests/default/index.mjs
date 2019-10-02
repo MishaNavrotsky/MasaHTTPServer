834a9e612ac88d4074d26d2900fa1e1c76653b02
MishaNavrotsky <MIsha172XDD@gmail.com>
2019-10-02 16:32:51 +0300

    small fix

--- server/requests/default/index.mjs
+++ server/requests/default/index.mjs
@@ -7,7 +7,6 @@ class def extends request {
         this.get.merge({
             path: /.+/,
             function: (req, res) => {
-                console.log(req.user);
                 res.status(404).send({
                     status: "error",
                     message: "Not found"
