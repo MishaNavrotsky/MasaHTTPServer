b3ac56edecce67cfa65c668351565310bf77f090
MishaNavrotsky <MIsha172XDD@gmail.com>
2019-10-10 00:55:04 +0300

    -

--- main.mjs
+++ main.mjs
@@ -3,11 +3,13 @@ import database from "./database"
 import server from "./server"
 import fs from "fs"
 
+const PORT = process.env.PORT || 5000
+
 (async function () {
     var db = new database("mongodb://localhost:27017/local");
-    await db.init();
+    // await db.init();
     var auth = new authentication(db);
     auth.init(fs.readFileSync("secret.txt"));
     var ser = new server(db, auth);
-    ser.init(1337);
+    ser.init(PORT);
 })();
