174564e1caa26b411cf09b3800b5d531fe7d76f0
MishaNavrotsky <MIsha172XDD@gmail.com>
2019-10-12 22:32:09 +0300

    ws

--- server/index.mjs
+++ server/index.mjs
@@ -24,6 +24,7 @@ class server {
         //CORS
         this.app.use((req, res, next) => {
             res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+            res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
             res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
         });
