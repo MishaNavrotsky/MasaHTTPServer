aede888bb9df20801eb9dc3d822e2b3a53f33d51
MishaNavrotsky <MIsha172XDD@gmail.com>
2019-10-15 15:16:09 +0300

    dick

--- server/index.mjs
+++ server/index.mjs
@@ -9,12 +9,29 @@ class server {
     constructor(db = {}, auth = {}) {
         this.db = db;
         this.auth = auth;
-        this.app = new express().use(cors({
-            origin: (or, cb)=>{
-                return cb(null, true) 
-            },
-            credentials: true
-        }));
+        this.app = new express();
+        this.app.use(function (req, res, next) {
+
+            // Website you wish to allow to connect
+            res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        
+            // Request methods you wish to allow
+            res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        
+            // Request headers you wish to allow
+            res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        
+            // Set to true if you need the website to include cookies in the requests sent
+            // to the API (e.g. in case you use sessions)
+            res.setHeader('Access-Control-Allow-Credentials', true);
+        
+            // Pass to next layer of middleware
+            next();
+        });
+            // .use(cors({
+            //     origin: "http://localhost:3000",
+            //     credentials: true
+            // }));
         this.requests = new requests({
             db: this.db,
             auth: this.auth
