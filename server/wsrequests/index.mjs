b3f0bb841a6f0b360d84346a687eb73633f85be2
MishaNavrotsky <MIsha172XDD@gmail.com>
2019-10-18 14:04:06 +0300

    ws message send

--- server/wsrequests/index.mjs
+++ server/wsrequests/index.mjs
@@ -1,15 +1,33 @@
 import expressWebSocket from 'express-ws'
 import messages from "./messages"
+import _ from "lodash"
 
+
+class WSConnection extends Object {
+    constructor(user, ws) {
+        this.user = user;
+        this.ws = ws;
+    }
+}
 class wsrequests extends Array {
     constructor(obj) {
         super();
         this.push(new messages(obj));
+        this.usersConnected = [];
     }
 
     init(express, defaultString) {
-        expressWebSocket(express);
+        const expressWS = expressWebSocket(express);
+        const wss = expressWS.getWss();
+
         express.ws(defaultString, (ws, req) => {
+            this.usersConnected.push({
+                user: req.user,
+                ws
+            });
+            ws.on("close", () => {
+                _.remove(this.usersConnected, value => value.user.username === req.user.username);
+            })
             ws.on("message", (message) => {
                 let obj = {};
                 try {
@@ -19,7 +37,7 @@ class wsrequests extends Array {
                 }
                 for (const webrequest of this) {
                     if (obj.path === webrequest.path) {
-                        webrequest.message.function(obj, ws);
+                        webrequest.message.function(obj, ws, req, this.usersConnected);
                         break;
                     }
                 }
--- server/wsrequests/messages/index.mjs
+++ server/wsrequests/messages/index.mjs
@@ -1,16 +1,49 @@
 import wsrequest from "../wsrequest"
+import {
+    Message
+} from "../../../database/schemas/message"
+import _ from "lodash"
 
 class messages extends wsrequest {
     constructor(obj) {
         super();
         this.db = obj.db
-        this.path = "messages"
+        this.usersConnected = obj.usersConnected;
+        this.path = "sendMessage"
         this.message.merge({
-            function: (obj, ws) => {
-                ws.send(JSON.stringify({
-                    status: "ok",
-                    data: "lolkek"
-                }))
+            function: (obj, ws, req, usersConnected) => {
+                let {
+                    to,
+                    data
+                } = obj;
+                const msg = new Message({
+                    from: req.user.username,
+                    to,
+                    data
+                });
+                this.db.addMessage(msg).then((smsg) => {
+                    ws.send(JSON.stringify({
+                        status: "ok",
+                    }));
+                    const sendTo = _.findLast(usersConnected, value => value.user.username === msg.to);
+                    if (sendTo) {
+                        sendTo.ws.send(
+                            JSON.stringify({
+                                status: "ok",
+                                path: "recieveMessage",
+                                from: req.user.username,
+                                data: msg.data
+                            })
+                        );
+                        smsg.received = true;
+                        smsg.save();
+                    }
+                }).catch(e => {
+                    ws.send(JSON.stringify({
+                        status: "error",
+                        message: e.message
+                    }))
+                });
             }
         })
     }
